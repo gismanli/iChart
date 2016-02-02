8d3f4eda949cabec347d4728d0dd0a0128d7d185
lili39 <lili39@baidu.com>
2016-02-02 12:06:18 +0800

    fixup! fix line

--- src/iChart.js
+++ src/iChart.js
@@ -392,32 +392,21 @@
 
         var cache = {};
         function tmpl(str, data){
-            // Figure out if we're getting a template, or if we need to
-            // load the template - and be sure to cache the result.
-            var fn = !/\W/.test(str) ?
-            cache[str] = cache[str] :
-
-            // Generate a reusable function that will serve as a template
-            // generator (and which will be cached).
-            new Function("obj",
-                "var p=[],print=function(){p.push.apply(p,arguments);};" +
-
-                // Introduce the data as local variables using with(){}
-                "with(obj){p.push('" +
-
-                // Convert the template into pure JavaScript
-                str
-                    .replace(/[\r\t\n]/g, " ")
-                    .split("<%").join("\t")
-                    .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-                    .replace(/\t=(.*?)%>/g, "',$1,'")
-                    .split("\t").join("');")
-                    .split("%>").join("p.push('")
-                    .split("\r").join("\\'") +
-                "');}return p.join('');"
-            );
-
-            // Provide some basic currying to the user
+            var fn = !/\W/.test(str)
+                ? cache[str] = cache[str]
+                : new Function("obj",
+                    "var p=[],print=function(){p.push.apply(p,arguments);};" +
+                    "with(obj){p.push('" +
+                    str
+                        .replace(/[\r\t\n]/g, " ")
+                        .split("<%").join("\t")
+                        .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+                        .replace(/\t=(.*?)%>/g, "',$1,'")
+                        .split("\t").join("');")
+                        .split("%>").join("p.push('")
+                        .split("\r").join("\\'") +
+                    "');}return p.join('');"
+                );
             return data ? fn( data ) : fn;
         }
         return tmpl(templateString,valuesObject);
@@ -620,7 +609,6 @@
             this.fit();
         },
         fit: function () {
-            console.log(this);
             this.startPoint = (this.display) ? this.fontSize : 0;
             this.endPoint = (this.display) ? this.height - (this.fontSize * 1.5) - 5 : this.height;
 
--- src/line.js
+++ src/line.js
@@ -6,55 +6,21 @@
     var methods = iChart.methods;
 
     var defaultConfig = {
-
-        ///Boolean - Whether grid lines are shown across the chart
         scaleShowGridLines : true,
-
-        //String - Colour of the grid lines
         scaleGridLineColor : "rgba(0,0,0,.05)",
-
-        //Number - Width of the grid lines
         scaleGridLineWidth : 1,
-
-        //Boolean - Whether to show horizontal lines (except X axis)
         scaleShowHorizontalLines: true,
-
-        //Boolean - Whether to show vertical lines (except Y axis)
         scaleShowVerticalLines: true,
-
-        //Boolean - Whether the line is curved between points
         bezierCurve : true,
-
-        //Number - Tension of the bezier curve between points
         bezierCurveTension : 0.4,
-
-        //Boolean - Whether to show a dot for each point
         pointDot : true,
-
-        //Number - Radius of each point dot in pixels
         pointDotRadius : 4,
-
-        //Number - Pixel width of point dot stroke
         pointDotStrokeWidth : 1,
-
-        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
         pointHitDetectionRadius : 20,
-
-        //Boolean - Whether to show a stroke for datasets
         datasetStroke : true,
-
-        //Number - Pixel width of dataset stroke
         datasetStrokeWidth : 2,
-
-        //Boolean - Whether to fill the dataset with a colour
         datasetFill : true,
-
-        //String - A legend template
-        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>",
-
-        //Boolean - Whether to horizontally center the label and point dot inside the grid
         offsetGridLines : false
-
     };
 
     iChart.Type.extend({
@@ -64,7 +30,7 @@
             this.PointClass = iChart.Point.extend({
                 offsetGridLines: this.options.offsetGridLines,
                 strokeWidth: this.options.pointDotStrokeWidth,
-                radius: this.options.pointDotRaius,
+                radius: this.options.pointDotRadius,
                 display: this.options.pointDot,
                 hitDetectionRaiudL: this.options.pointHitDetectionRadius,
                 ctx: this.chart.ctx,
@@ -181,9 +147,9 @@
                     ctx.stroke();
                 }
 
-                if (this.options.datesetFill && pointsWithValues.length > 0) {
+                if (this.options.datasetFill && pointsWithValues.length > 0) {
                     ctx.lineTo(pointsWithValues[pointsWithValues.length - 1].x, this.scale.endPoint);
-                    ctx.lineTo(pointsWithValues[0].x, this.sacal.endPoint);
+                    ctx.lineTo(pointsWithValues[0].x, this.scale.endPoint);
                     ctx.fillStyle = dataset.fillColor;
                     ctx.closePath();
                     ctx.fill();
